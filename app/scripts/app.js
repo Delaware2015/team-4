781fedb377d316c7b0271fd188960a2c7fe0277e
NghiaTTran <nghiattran3@gmail.com>
2015-10-30 21:00:48 -0400

    something

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,12 +1,49 @@
+
+
+
 'use strict';
 
 /**
  * @ngdoc overview
- * @name cfgApp
+ * @name mySiteApp
  * @description
- * # cfgApp
+ * # mySiteApp
  *
  * Main module of the application.
  */
 angular
-  .module('cfgApp', []);
+  .module('cfgApp', [
+    'ui.router',
+  ])
+  .config(function ($stateProvider) {
+    // $routeProvider
+    //   .when('/', {
+    //     templateUrl: 'views/main.html',
+    //     controller: 'MainCtrl',
+    //     controllerAs: 'main'
+    //   })
+    //   .when('/about', {
+    //     templateUrl: 'views/about.html',
+    //     controller: 'AboutCtrl',
+    //     controllerAs: 'about'
+    //   })
+    //   .otherwise({
+    //     redirectTo: '/'
+    //   });
+    // $urlRouterProvider
+    //   .otherwise("/");
+
+    $stateProvider
+
+      .state('login', 
+      {
+        url: "/",
+        templateUrl: 'views/login.html',
+        data: {
+        },
+        controller: 'MainCtrl',
+      })
+
+    
+  });
+
