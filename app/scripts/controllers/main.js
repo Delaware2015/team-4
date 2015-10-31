1a9cc077f80d6c50bbcb154455ff56b52b070281
NghiaTTran <nghiattran3@gmail.com>
2015-10-30 21:33:15 -0400

    login page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,20 +8,37 @@
  * Controller of the cfgApp
  */
 angular.module('cfgApp')
-  .controller('MainCtrl', function ($scope, parseServices) {
+  .controller('MainCtrl', function ($scope, parseServies) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
+    parseServies.init();
+
+    $scope.signup = function(credentials)
+    {
+        var credentials = {
+          username: "test",
+          password: "test",
+        }
+        parseServies.signup(credentials).then(function(data){
+        if (!data.results.error) {
+          console.log(data.results);
+        } else{
+          console.log(data.results.error);
+          $scope.login_error = 'Your username or password is incorrect';
+        }
+      })
+    }
+   
     $scope.login = function(credentials)
     {
-    	parseServies.login(credentials).then(function(data){
+    console.log(credentials);
+  	parseServies.login(credentials).then(function(data){
       if (!data.results.error) {
-        console.log(data.results);
       } else{
-        console.log(data.results.error);
         $scope.login_error = 'Your username or password is incorrect';
       }
     })
